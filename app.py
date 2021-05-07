469f3823c1a4c634ec8f8fd1b095eb6ca8d01f20
SWATHI.S <66115090+Swathi201801150@users.noreply.github.com>
2021-05-07 12:32:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import sklearn
+import joblib
+model = joblib.load('/content/Sentiment_model')
+st.title('Sentiment Analysis')
+ip = st.text_input("Enter the Tweet")
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
